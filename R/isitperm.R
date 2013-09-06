77a1c90446e093e1b2fc4ecc5b87750022ffc48f
AmeliaMN <amelia.a.mcnamara@gmail.com>
2013-09-06 13:15:26 -0700

    "fixed the minus issue?"

--- R/isitperm.R
+++ R/isitperm.R
@@ -9,20 +9,19 @@
 isitperm <- function(data, answer){
   require(gtools)
   fnList <- ls("package:base")
-  badfunctions <- dget("badfunctions.robj")
+ # badfunctions <- dget("badfunctions.robj")
   listNoSideEffects <- fnList[fnList %in% badfunctions==FALSE]
-  listNoSideEffects <- listNoSideEffects[-c(2:3,8:9, 13:26, 28:32,34:36,38:42,45,47, 49, 51:52, 62:63, 73:74, 78:79, 81)]
+  orderings <- permutations(length(data), length(data), 1:length(data))
   
-  
-  for (procName in listNoSideEffects){
-    orderings <- permutations(length(data), length(data), 1:length(data))
-    readline(prompt = paste0("hit Enter to try ", deparse(substitute(procName)), ":")) 
-    for (i in 1:dim(orderings)[1]){
-      trialAnswer <- tryCatch(
-        do.call(procName, data[orderings[i,]])
-        , error = function(e) as.character(e)
-      )
-    }
+  for (i in 1:dim(orderings)[1]){
+    for (procName in listNoSideEffects){
+      readline(prompt = paste0("hit Enter to try ", deparse(substitute(procName)), ":")) 
+        print(data[orderings[i,]])
+        trialAnswer <- tryCatch(
+          do.call(procName, data[orderings[i,]])
+          , error = function(e) as.character(e)
+        )
+      }
   }
   
 }
