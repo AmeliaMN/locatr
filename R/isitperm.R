41be2d6f3a8b4a9d97ef151eb4f2236fedf8be37
AmeliaMN <amelia.a.mcnamara@gmail.com>
2013-09-17 14:12:59 -0700

    "give the choice between prompting at each trial and just printing names"

--- R/isitperm.R
+++ R/isitperm.R
@@ -6,14 +6,19 @@
 #' @param answer the answer you expect
 #' @export
 
-isitperm <- function(data, answer){
+isitperm <- function(data, answer, ask=TRUE){
   require(gtools)
   listNoSideEffects <- goodfunctions
   orderings <- permutations(length(data), length(data), 1:length(data))
   
   for (i in 1:dim(orderings)[1]){
     for (procName in listNoSideEffects){
-      readline(prompt = paste0("hit Enter to try ", deparse(substitute(procName)), ":")) 
+      if(ask){
+        readline(prompt = paste0("hit Enter to try ", deparse(substitute(procName)), ":")) 
+      }
+      else {
+        print(procName)
+      }
       timeOut <- function (expr, ...)  {
         on.exit(setTimeLimit())
         setTimeLimit(...)
